eefc1b126269a52aeeb3422c7c87636e60bcd1ee
Juan Dorado <fdorado985@gmail.com>
2020-08-25 19:17:45 -0500

    Implement the view models for the model and the UI

---
+++ Stocks/View Models/StockListViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  StockListViewModel.swift
+//  Stocks
+//
+//  Created by Juan Francisco Dorado Torres on 25/08/20.
+//  Copyright © 2020 Juan Francisco Dorado Torres. All rights reserved.
+//
+
+import Foundation
+
+class StockListViewModel {
+
+  var searchTerm = ""
+  var stocks = [StockViewModel]()
+}
