de9899eb449935c6c2b4f6c7ff9e89dd434f0b11
Juan Dorado <fdorado985@gmail.com>
2020-08-25 19:33:24 -0500

    setting up the main user interface

--- Stocks/ContentView.swift
+++ Stocks/ContentView.swift
@@ -9,13 +9,38 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+
+  @State private var searchTerm = ""
+
+  init() {
+    UINavigationBar.appearance().backgroundColor = .black
+    UINavigationBar.appearance().largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+    UITableView.appearance().backgroundColor = .black
+    UITableViewCell.appearance().backgroundColor = .black
+  }
+
+  var body: some View {
+    NavigationView {
+      ZStack(alignment: .leading) {
+        Color.black
+        Text("January 5 2020")
+          .font(.custom("Arial", size: 32))
+          .fontWeight(.bold)
+          .foregroundColor(Color.gray)
+          .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0))
+          .offset(y: -400)
+
+        SearchView(searchTerm: $searchTerm)
+          .offset(y: -350)
+      }
+      .navigationBarTitle("Stocks")
     }
+    .edgesIgnoringSafeArea(Edge.Set(.bottom))
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
